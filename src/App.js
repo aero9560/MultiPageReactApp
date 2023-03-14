07466e53d4963e5ab6b4b23cc379f851750c55dc
Abhishak <abhi.bhardwaj122@gmail.com>
2023-03-14 12:08:56 +0530

    added readme

--- src/App.js
+++ src/App.js
@@ -2,11 +2,20 @@ import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 
 import HomePage from './pages/Home';
 import ProductsPage from './pages/Products';
+import ErrorPage from './pages/Error';
+import RootLayout from './pages/Root';
 
 
 const router = createBrowserRouter([
-  { path: '/', element: <HomePage /> },
-  { path: '/products', element: <ProductsPage /> },
+  {
+    path: '/',
+    element: <RootLayout />,
+    errorElement: <ErrorPage />,
+    children: [
+      { path: '/', element: <HomePage /> },
+      { path: '/products', element: <ProductsPage /> },
+    ],
+  }
 ]);
 
 function App() {
